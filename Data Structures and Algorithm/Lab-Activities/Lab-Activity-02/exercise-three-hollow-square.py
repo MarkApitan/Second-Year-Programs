fe40094cdd01e9d0c05d8ad4befbaa4348b35816
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-09-30 13:07:12 +0800

    add a function in exercise three and created a jupyter file

--- Data Structures and Algorithm/Lab-Activities/Lab-Activity-02/exercise-three-hollow-square.py
+++ Data Structures and Algorithm/Lab-Activities/Lab-Activity-02/exercise-three-hollow-square.py
@@ -1,13 +1,15 @@
+def create_square(length):
+    for i in range (length):
+        if i == 0 or i == length -1:
+            print (("*") * length)
+        else:
+            print(("*") + (" ")* (length - 2) + ("*"))
+
 is_on =  True
 while is_on:
     try:
         length = int(input("Enter the side length of the square: "))
+        create_square(length)
         is_on = False
     except ValueError:
         print("Invalid Input. Please enter a valid number.")
-        
-for i in range (length):
-    if i == 0 or i == length -1:
-        print (("*") * length)
-    else:
-        print(("*") + (" ")* (length - 2) + ("*"))
